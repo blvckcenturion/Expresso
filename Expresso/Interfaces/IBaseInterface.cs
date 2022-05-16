407b1c8a00e8e7c1912bf4421b4e28ed29edf2c8
blvckcenturion <blvckcenturion@gmail.com>
2022-05-15 21:34:43 -0400

    Initial Commit

---
+++ Expresso/Interfaces/IBaseInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Expresso.Interfaces
+{
+    public interface IBaseInterface<T>
+    {
+        #region Methods
+        DataTable Select();
+        int Insert(T t);
+        int Update(T t);
+        int Delete(T t);
+        #endregion
+    }
+}
---
+++ Expresso/Model/BaseTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expresso.Model
+{
+    public class BaseTable
+    {
+        #region Properties
+        public byte Status { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public int UserID { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// GET
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="registerDate"></param>
+        /// <param name="lastUpdate"></param>
+        /// <param name="userID"></param>
+        public BaseTable(byte status, DateTime registerDate, DateTime lastUpdate, int userID)
+        {
+            Status = status;
+            RegisterDate = registerDate;
+            LastUpdate = lastUpdate;
+            UserID = userID;
+        }
+
+        /// <summary>
+        /// EMPTY
+        /// </summary>
+        public BaseTable()
+        {
+        }
+        #endregion
+
+    }
+}
