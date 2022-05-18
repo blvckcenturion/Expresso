e24227a794cc10127b861c36f00a08c062b1bb0b
blvckcenturion <blvckcenturion@gmail.com>
2022-05-17 23:05:24 -0400

    Initial WPF Implementation

--- Expresso/Implementation/ProductImpl.cs
+++ Expresso/Implementation/ProductImpl.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Expresso.Interfaces;
 using Expresso.Model;
+using System.Data.SqlClient;
 
 namespace Expresso.Implementation
 {
@@ -13,22 +14,76 @@ namespace Expresso.Implementation
     {
         public int Delete(Product t)
         {
-            throw new NotImplementedException();
+            string query = @"UPDATE Product SET status = 0, lastUpdate = CURRENT_TIMESTAMP, userID = @userID
+                             WHERE id = @id";
+            SqlCommand command = CreateBasicCommand(query);
+            command.Parameters.AddWithValue("@id", t.Id);
+            command.Parameters.AddWithValue("@userID", SessionClass.sessionUserID);
+            try
+            {
+                return ExecuteBasicCommand(command);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public int Insert(Product t)
         {
-            throw new NotImplementedException();
+            string query = @"INSERT INTO Product(productName, basePrice, productCategoryID, productDescription, userID)
+                            VALUES(@productName, @basePrice, @productCategoryID, @productDescription, @userID)";
+            SqlCommand command = CreateBasicCommand(query);
+            command.Parameters.AddWithValue("@productName", t.ProductName);
+            command.Parameters.AddWithValue("@basePrice", t.BasePrice);
+            command.Parameters.AddWithValue("@productCategoryId", t.ProductCategoryId);
+            command.Parameters.AddWithValue("@productDescription", t.ProductDescription);
+            command.Parameters.AddWithValue("@userID", SessionClass.sessionUserID);
+            try
+            {
+                return ExecuteBasicCommand(command);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public DataTable Select()
         {
-            throw new NotImplementedException();
+            string query = @"SELECT id, productName AS 'Nombre del Producto', productDescription AS 'Descripcion del Producto', basePrice AS 'Precio Base', productCategoryID AS 'ID De la Categoria', registerDate AS 'Fecha de Creacion'
+                             FROM Product
+                             WHERE status=1";
+            SqlCommand command = CreateBasicCommand(query);
+            try
+            {
+                return ExecuteDataTableCommand(command);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public int Update(Product t)
         {
-            throw new NotImplementedException();
+            string query = @"UPDATE Product
+                             SET productName=@productName, productDescription=@productDescription, basePrice=@basePrice lastUpdate=CURRENT_TIMESTAMP, userID=@userID
+                             WHERE id=@id";
+            SqlCommand command = CreateBasicCommand(query);
+            command.Parameters.AddWithValue("@productName", t.ProductName);
+            command.Parameters.AddWithValue("@productDescription", t.ProductDescription);
+            command.Parameters.AddWithValue("@basePrice", t.BasePrice);
+            command.Parameters.AddWithValue("@id", t.Id);
+            command.Parameters.AddWithValue("@userID", SessionClass.sessionUserID);
+            try
+            {
+                return ExecuteBasicCommand(command);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
---
+++ ExpressoWPF/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpressoWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void btnPointOfSale_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnPointOfSale;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Nueva Orden";
+            }
+        }
+
+        private void btnPointOfSale_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnPreviousSales_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnPreviousSales;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Ordenes Previas";
+            }
+        }
+
+        private void btnPreviousSales_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnProducts_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnProducts_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnProducts;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Productos";
+            }
+        }
+
+        private void btnLocations_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnLocations;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Ubicaciones";
+            }
+        }
+
+        private void btnLocations_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnSettings_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnSettings;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Configuracion";
+            }
+        }
+
+        private void btnSettings_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+        // End: MenuLeft PopupButton //
+
+        // Start: Button Close | Restore | Minimize 
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        // End: Button Close | Restore | Minimize
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Home.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void btnLocations_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Dashboard.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
